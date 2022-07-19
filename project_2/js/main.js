2e04e003ea15c27311e63c2f6516f0e5a58753e8
Yuliia Pasiechnyk <yuliia.pasiechnyk@gmail.com>
2022-07-19 16:30:26 +0300

    change project

--- project_2/js/main.js
+++ project_2/js/main.js
@@ -1,3 +1,4 @@
+// hero slide
 document.addEventListener("DOMContentLoaded", function () {
   let splide = new Splide("#splide-banner", {
     arrows: false,
@@ -9,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide.mount();
 });
-
+// news slide
 document.addEventListener("DOMContentLoaded", function () {
   let splide = new Splide("#splide-news", {
     type: "loop",
@@ -23,3 +24,59 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide.mount();
 });
+
+// map
+const displayMap = document.getElementById("map");
+
+function initMap(newLat, newLng) {
+  const myLatLng = { lat: newLat, lng: newLng };
+
+  const map = new google.maps.Map(displayMap, {
+    zoom: 13,
+    center: myLatLng,
+    disableDefaultUI: true,
+    styles: [
+      {
+        stylers: [
+          {
+            saturation: -90,
+          },
+        ],
+      },
+    ],
+  });
+
+  const image = new google.maps.MarkerImage(
+    "..//assets/image/main_map.png",
+    null, // size
+    null, // origin
+    new google.maps.Point(8, 8), // anchor (move to center of marker)
+    new google.maps.Size(25, 25) // scaled size (required for Retina display icon)
+  );
+
+  const marker = new google.maps.Marker({
+    flat: true,
+    optimized: false,
+    position: myLatLng,
+    map: map,
+    icon: image,
+    title: "We are here!",
+  });
+  marker.setMap(map);
+}
+
+window.initMap = initMap(50.458309842219066, 30.46494879270167);
+
+/*let submitBtn = document.querySelector(".contacts__form");
+let submitBtn = document.querySelector(".contacts__btn");
+let nameInput = document.querySelector(".contacts__nameinput");
+let mailInput = document.querySelector(".contacts__mailinput");
+
+submitBtn.addEventListener("click", function () {
+  let mail;
+  mail = mailInput.value;
+  let name;
+  name = nameInput.value;
+  alert("Your name: " + name);
+  alert("Your email: " + mail);
+});*/
